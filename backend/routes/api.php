1648550fce20531804daf30ae6deacfc17d1a53c
Vitor Hugo <vitorhugosg@gmail.com>
2018-10-19 17:28:20 -0300

    Segue

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::prefix('auth')->middleware('cors')->group(function(){
+Route::prefix('auth')->group(function(){
 	Route::post('login', 'Users\AuthController@login');
 	Route::post('register', 'Users\AuthController@register');
 	
