d9098bf1017152a659f9193e9a00448d2dd3e116
Vitor Hugo <vitorhugosg@gmail.com>
2018-10-19 19:18:00 -0300

    Segue

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,12 +1,19 @@
 <?php
 
 return [
-/*
-'permissions' => [
-'dominio1.dev',
-'dominio2.com.br',
-'www.dominio1.com.br',
-],
- */
-    'permissions' => '*', // Libera todos domínios
-];
+     /*
+     |--------------------------------------------------------------------------
+     | Laravel CORS
+     |--------------------------------------------------------------------------
+     |
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+     | to accept any value.
+     |
+     */
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
+    'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+]
