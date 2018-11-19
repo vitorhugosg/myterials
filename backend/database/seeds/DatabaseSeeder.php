874447a8bef1682f538d1e6bfae4131ed34b2467
Vitor Hugo <vitorhugosg@gmail.com>
2018-11-19 17:18:04 -0200

    Segue

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,155 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(database\seeds\uofm::class);
-        $this->call(database\seeds\uofm_type::class);
+        $data = [
+    		//CORRIDO
+    		[
+    			'uofm_type_id' => 1,
+    			'name' => 'EACH',
+    			'base_factor' => 1,
+    			'base_multiply' => 1
+    		],
+    		//CORRIDO
+    		[
+    			'uofm_type_id' => 2,
+    			'name' => 'MM',
+    			'base_factor' => 1,
+    			'base_multiply' => 1
+    		],
+    		[
+    			'uofm_type_id' => 2,
+    			'name' => 'Foot',
+    			'base_factor' => 1,
+    			'base_multiply' => 304.8
+    		],
+    		[
+    			'uofm_type_id' => 2,
+    			'name' => 'Meters',
+    			'base_factor' => 1,
+    			'base_multiply' => 1000
+    		],
+    		[
+    			'uofm_type_id' => 2,
+    			'name' => 'Inches',
+    			'base_factor' => 1,
+    			'base_multiply' => 25.4
+    		],
+    		//Quadrados²
+    		[
+    			'uofm_type_id' => 3,
+    			'name' => 'Meters²',
+    			'base_factor' => 1,
+    			'base_multiply' => 1
+    		],
+    		[
+    			'uofm_type_id' => 3,
+    			'name' => 'FT²',
+    			'base_factor' => 1,
+    			'base_multiply' => 10.7639
+    		],
+    		[
+    			'uofm_type_id' => 3,
+    			'name' => 'YD²',
+    			'base_factor' => 1,
+    			'base_multiply' => 0.836127
+    		],
+    		[
+    			'uofm_type_id' => 3,
+    			'name' => 'KM²',
+    			'base_factor' => 1,
+    			'base_multiply' => 1000000
+    		],
+    		//VOLUME
+    		[
+    			'uofm_type_id' => 4,
+    			'name' => 'CM³',
+    			'base_factor' => 1,
+    			'base_multiply' => 1
+    		],
+    		[
+    			'uofm_type_id' => 4,
+    			'name' => 'IN³',
+    			'base_factor' => 1,
+    			'base_multiply' => 16.3871
+    		],
+    		[
+    			'uofm_type_id' => 4,
+    			'name' => 'FT³',
+    			'base_factor' => 1,
+    			'base_multiply' => 28316.8
+    		],
+    		[
+    			'uofm_type_id' => 4,
+    			'name' => 'GAL',
+    			'base_factor' => 1,
+    			'base_multiply' => 3785.41
+    		],
+    		[
+    			'uofm_type_id' => 4,
+    			'name' => 'ML',
+    			'base_factor' => 1,
+    			'base_multiply' => 1
+    		],
+    		[
+    			'uofm_type_id' => 4,
+    			'name' => 'L',
+    			'base_factor' => 1,
+    			'base_multiply' => 1000
+    		],
+
+    		//PESO
+    		//
+    		[
+    			'uofm_type_id' => 5,
+    			'name' => 'GG',
+    			'base_factor' => 1,
+    			'base_multiply' => 1
+    		],
+    		[
+    			'uofm_type_id' => 5,
+    			'name' => 'KG',
+    			'base_factor' => 1,
+    			'base_multiply' => 1000
+    		],
+    		[
+    			'uofm_type_id' => 5,
+    			'name' => 'TONS',
+    			'base_factor' => 1,
+    			'base_multiply' => 1000000
+    		],
+    		[
+    			'uofm_type_id' => 5,
+    			'name' => 'OZ',
+    			'base_factor' => 1,
+    			'base_multiply' => 28.3495
+    		],
+    		[
+    			'uofm_type_id' => 5,
+    			'name' => 'LB',
+    			'base_factor' => 1,
+    			'base_multiply' => 453.592
+    		],
+    	];
+
+    	
+        DB::table('uofm')->insert($data);
+        $data = [
+    		[
+    			'name' => 'Unity',
+    		],
+    		[
+    			'name' => 'Linear'
+    		],
+    		[
+    			'name' => 'Area'
+    		],
+    		[
+    			'name' => 'Volume'
+    		],
+    		[
+    			'name' => 'Weight'
+    		]
+    	];
+        DB::table('uofm_type')->insert($data);
     }
 }
