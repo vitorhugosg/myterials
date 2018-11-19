39daea02ed47938344b8f94b2b645abca12539a9
Vitor Hugo <vitorhugosg@gmail.com>
2018-11-19 17:12:53 -0200

    Segue

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(uofm::class);
-        // $this->call(uofm_type::class);
+        $this->call(uofm::class);
+        $this->call(uofm_type::class);
     }
 }
