6935d19ffd205fc3f51576d27ab42952d1ffda9e
Vitor Hugo <vitorhugosg@gmail.com>
2018-11-19 18:32:53 -0200

    Segue

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
-header("Cache-Control: no-cache, must-revalidate");
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+// header("Cache-Control: no-cache, must-revalidate");
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
 /*
 |--------------------------------------------------------------------------
 | API Routes
