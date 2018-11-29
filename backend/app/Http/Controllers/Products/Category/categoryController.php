d0232cf3619c61ae9b7ed8e6c35b7b3ba0127878
Vitor Hugo <vitorhugosg@gmail.com>
2018-11-29 19:27:54 -0200

    Segue

--- backend/app/Http/Controllers/Products/Category/categoryController.php
+++ backend/app/Http/Controllers/Products/Category/categoryController.php
@@ -17,7 +17,9 @@ class categoryController extends Controller
     	$user = $request->user();
     	if ($user->companyes()->find($idCompany)) {
     		$collections =  Collection::where('company_id', $idCompany)->where('status', 1)->get();
+            dump($collections);
             if (!empty($collections)) {
+
                 foreach ($collections as $key => $value) {
                     $collectionsId[] = $collections[$key]['id'];
                 }
